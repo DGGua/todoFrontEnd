79e200159afbfaccdb25212362dc4c87bfe64e9f
dg_gua@foxmail.com <dg_gua@foxmail.com>
2023-01-05 14:45:10 +0800

    登录注册样式

--- src/pages/tsx/login.tsx
+++ src/pages/tsx/login.tsx
@@ -1,10 +1,16 @@
-import { Button, Form, Input, Modal } from "antd-mobile";
+import { Button, Card, Form, Input, Modal } from "antd-mobile";
 import { Link, useNavigate } from "react-router-dom";
 import "../scss/login.scss";
 import { userService } from "../../service/userService";
+import { useState } from "react";
 export default function Login() {
   const navigate = useNavigate();
-  function handleLogin(id: string, password: string) {
+  const [loginInfo, setLoginInfo] = useState<{
+    id: string;
+    password: string;
+  }>();
+  function handleLogin() {
+    const { id = "", password = "" } = loginInfo || {};
     userService.login(id, password).then((response) => {
       if (response.data.msg === "成功") {
         navigate("/home");
@@ -18,37 +24,37 @@ export default function Login() {
     <div className="page page-login">
       <div className="panel-login">
         <h1>登录</h1>
-        <Form
-          layout="horizontal"
-          mode="card"
-          onFinish={(val: { id: string; password: string }) =>
-            handleLogin(val.id, val.password)
-          }
-        >
-          <Form.Item
-            label="账号"
-            name="id"
-            rules={[{ required: true, message: "账号不能为空" }]}
+        <Card>
+          <Form
+            layout="horizontal"
+            mode="card"
+            onValuesChange={(_, values) => setLoginInfo(values)}
           >
-            <Input placeholder="请输入" />
-          </Form.Item>
-          <Form.Item
-            label="密码"
-            name="password"
-            rules={[{ required: true, message: "请输入密码" }]}
-          >
-            <Input placeholder="请输入" type="password" />
-          </Form.Item>
-          <Link to={"/register"} className="link-to-register">
-            没有账号？点击注册
-          </Link>
-          <Link to={"/home"} className="link-to-register">
-            临时入口，直接进去
-          </Link>
-          <Button block type="submit" color="primary" size="large">
-            登录
-          </Button>
-        </Form>
+            <Form.Item
+              label="账号"
+              name="id"
+              rules={[{ required: true, message: "账号不能为空" }]}
+            >
+              <Input placeholder="请输入" />
+            </Form.Item>
+            <Form.Item
+              label="密码"
+              name="password"
+              rules={[{ required: true, message: "请输入密码" }]}
+            >
+              <Input placeholder="请输入" type="password" />
+            </Form.Item>
+          </Form>
+        </Card>
+        <Link to={"/register"} className="link-to-register">
+          没有账号？点击注册
+        </Link>
+        <Link to={"/home"} className="link-to-register">
+          临时入口，直接进去
+        </Link>
+        <Button block color="primary" size="large" onClick={handleLogin}>
+          登录
+        </Button>
       </div>
       <u
         className="about"
