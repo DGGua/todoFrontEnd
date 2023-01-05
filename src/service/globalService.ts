781e172249667fbe0acd8388903730188418cf83
dg_gua@foxmail.com <dg_gua@foxmail.com>
2023-01-05 14:33:52 +0800

    登录检测

--- src/service/globalService.ts
+++ src/service/globalService.ts
@@ -1,7 +1,13 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import { ResTemp } from "../model/item";
 
 export const localAxios = axios.create({
-  baseURL: "https://mock.apifox.cn/m1/2145942-0-default",
-//   baseURL: "http://192.168.1.101:8081",
+  //   baseURL: "https://mock.apifox.cn/m1/2145942-0-default",
+  baseURL: "http://192.168.1.101:8080",
   withCredentials: true,
+});
+
+localAxios.interceptors.response.use((res) => {
+  if (res.data.msg === "未登录") window.location.href = "/login";
+  return Promise.resolve(res);
 });
