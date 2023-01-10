520c57c5be6afd5dbb2747c06a4c34dca3117b96
DGGua <41000029+DGGua@users.noreply.github.com>
2023-01-10 18:01:11 +0800

    Update globalService.ts

--- src/service/globalService.ts
+++ src/service/globalService.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 export const localAxios = axios.create({
-  //   baseURL: "https://mock.apifox.cn/m1/2145942-0-default",
-  baseURL: "http://todo.dggua.top/api",
-  // baseURL: "http://192.168.1.101:8080",
+  // baseURL: "https://mock.apifox.cn/m1/2145942-0-default",
+  //   baseURL: "http://todo.dggua.top/api",
+  //   baseURL: "http://192.168.1.101:8080",
+  baseURL: "http://todob.dggua.top",
   withCredentials: true,
 });
 
